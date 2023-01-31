06fd731fb553161dbb38bf28d036c5c2ce8d37a8
Ghazy <dianghazy@protonmail.com>
2023-01-31 18:32:41 +0700

    update sveltekit

---
+++ _app/immutable/chunks/1-8d23e438.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-02c1a26b.js";export{t as component};
---
+++ _app/immutable/chunks/2-619a2016.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-4861c6c3.js";export{t as component};
